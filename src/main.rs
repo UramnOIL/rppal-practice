bdec4140f5b3e2211748fd1bd538c0d6ce0bef97
uramnoil <uramnoil@outlook.jp>
2020-10-31 01:50:52 +0900

    ハードコード排除

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use rppal::gpio::Gpio;
 use std::thread::sleep;
 use std::time::Duration;
 
+const GPIO_PIN_DHT11: u8 = 23;
+
 fn main() {
-    let mut dht11 = dht11::new(Gpio::new().unwrap().get(23).unwrap());
+    let mut dht11 = dht11::new(Gpio::new().unwrap().get(GPIO_PIN_DHT11).unwrap());
 
     loop {
         println!("{:?}", dht11.read().unwrap());
